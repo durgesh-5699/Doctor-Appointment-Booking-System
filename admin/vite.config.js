20b6f3e7d193f3ef13e0582b33ed7348be669e0a
Durgesh bhatt <durgeshbhatt5699@gmil.com>
2025-06-03 22:58:50 +0530

    completed

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcssPostcss from '@tailwindcss/postcss'
-import autoprefixer from 'autoprefixer'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcssPostcss(),
-        autoprefixer()],
-  server : {port:5174},
+  plugins: [react()],
+  server: { port: 5174 },
 })
