fa73e4cf0bfc407216465c351eb52d2423b27dee
Durgesh Bhatt <151530880+durgesh-5699@users.noreply.github.com>
2025-06-19 21:26:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from "express"
-import cors from "cors" //!Cross-Origin Resource Sharing
+import cors from "cors" 
 import 'dotenv/config'
 import connectDB from "./config/mongodb.js"
 import connectCloudinary from "./config/cloudinary.js"
@@ -22,7 +22,6 @@ app.use(cors())
 app.use('/api/admin' , adminRouter)
 app.use('/api/doctor' , doctorRoute)
 app.use('/api/user',userRouter)
-//localhost:5000/api/admin/add-doctor
 
 app.get("/" , (req, res)=>{
     res.send("api is working") ;
